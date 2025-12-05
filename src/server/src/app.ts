bdc6d06baba05bc9c3f9fea49ed212189289aac1
Tosif Kankod <73471650+Tosifkankod@users.noreply.github.com>
2025-12-05 18:49:47 +0530

    feat: server- added not found route

--- src/server/src/app.ts
+++ src/server/src/app.ts
@@ -1,7 +1,9 @@
-import express, { Application } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 import path from 'path';
 import router from './router/apiRouter';
 import globalErrorHandler from './middleware/globalErrorHandler';
+import responseMessage from './constant/responseMessage';
+import httpError from './utils/httpError';
 
 const app: Application = express();
 
@@ -9,9 +11,18 @@ const app: Application = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../', 'public')));
 
+
 // Routes
 app.use('/api/v1', router)
 
+// 404 Handler
+app.use((req: Request, _: Response, next: NextFunction) => {
+    try {
+        throw new Error(responseMessage.NOT_FOUND('route'))
+    } catch (error) {
+        httpError(next, error, req, 404)
+    }
+})
 
 // global error handler
 app.use(globalErrorHandler);
