4a9d203b924bcf05c9c584d7519f35aad98870b2
Tosif Kankod <73471650+Tosifkankod@users.noreply.github.com>
2025-12-11 21:44:17 +0530

    feat: server-added backend health api

--- src/server/src/controller/apiController.ts
+++ src/server/src/controller/apiController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express"
 import httpResponse from "../utils/httpResponse";
 import responseMessage from "../constant/responseMessage";
 import httpError from "../utils/httpError";
+import quicker from "../utils/quicker";
 
 
 export default {
@@ -9,6 +10,18 @@ export default {
         try {
             throw new Error('this is error');
             httpResponse(req, res, 200, responseMessage.SUCCESS, { id: "i123" });
+        } catch (error) {
+            httpError(next, error, req, 500);
+        }
+    },
+    health: (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const healthData = {
+                application: quicker.getApplicationHealth(),
+                system: quicker.getSystemHealth(),
+                timestamp: Date.now()
+            }
+            httpResponse(req, res, 200, responseMessage.SUCCESS, healthData);
         } catch (error) {
             httpError(next, error, req, 500);
         }
