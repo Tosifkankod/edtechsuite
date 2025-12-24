87ace937ff8b072f11edc2031679b3792d760a5d
Tosif Kankod <73471650+Tosifkankod@users.noreply.github.com>
2025-12-24 19:19:06 +0530

    fix: added input form

--- src/client/src/components/ui/SelectInput.tsx
+++ src/client/src/components/ui/SelectInput.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
 
 type SelectOption = {
     label: string;
@@ -8,40 +8,57 @@ type SelectOption = {
 };
 
 type SelectInputChildProps = {
-    name: string,
-    label?: string,
-    required?: boolean,
-    className?: string,
-    labelClassName?: string,
-    option: SelectOption[]
-}
+    name: string;
+    label?: string;
+    required?: boolean;
+    className?: string;
+    labelClassName?: string;
+    option: SelectOption[];
+    onChange?: React.ChangeEventHandler<HTMLSelectElement>;
+};
 
-const SelectInput: React.FC<SelectInputChildProps> = ({ name, label, required, className, labelClassName, option }) => {
+const SelectInput: React.FC<SelectInputChildProps> = ({
+    name,
+    label,
+    required,
+    className,
+    labelClassName,
+    option,
+    onChange,
+}) => {
     const mergedInputClass = twMerge(
         clsx(
-            "bg-neutral-secondary-medium border border-gray-200 bg-gray-50 text-heading rounded-md focus:ring-brand focus:border-brand block text-sm w-full px-3 py-2 my-1  placeholder:text-body",
+            "bg-neutral-secondary-medium border border-gray-200 bg-gray-50 text-heading rounded-md focus:ring-brand focus:border-brand block text-sm w-full px-3 py-2 my-1 placeholder:text-body",
             className
         )
-    )
+    );
 
     return (
         <div className="w-full">
-            {label &&
-                <label
-                    className={twMerge("text-sm font-medium", labelClassName)}
-                >
+            {label && (
+                <label className={twMerge("text-sm font-medium", labelClassName)}>
                     {label}
                 </label>
-            }
-            <select id="countries" name={name} required={required} className={mergedInputClass}>
-                {
-                    option && option.map((item) => {
-                        return <option value={item.value} >{item.label}</option>
-                    })
-                }
+            )}
+
+            <select
+                name={name}
+                required={required}
+                className={mergedInputClass}
+                onChange={onChange}
+            >
+                {option.map((item) => (
+                    <option
+                        key={item.value}
+                        value={item.value}
+                        disabled={item.disabled}
+                    >
+                        {item.label}
+                    </option>
+                ))}
             </select>
         </div>
-    )
-}
+    );
+};
 
-export default SelectInput
+export default SelectInput;
--- src/client/src/modules/course/pages/CourseEdit.tsx
+++ src/client/src/modules/course/pages/CourseEdit.tsx
@@ -1,10 +1,40 @@
-import { useState } from "react"
+import { useState, type ChangeEvent, type FormEvent } from "react"
 import Input from "../../../components/ui/Input"
 import SelectInput from "../../../components/ui/SelectInput";
 import { Save } from "lucide-react";
 
+const courseStatus = [
+    {
+        label: 'active',
+        value: 'active'
+    },
+    {
+        label: 'Inactive',
+        value: 'inactive'
+    }
+]
+
 const CourseEdit = () => {
-    const [name, setName] = useState('');
+    const [courseData, setCourseData] = useState({
+        courseName: "",
+        courseSlug: "",
+        courseDuration: "",
+        courseFee: '',
+        courseStatus: null,
+        courseDescription: "",
+    });
+
+    const handleOnChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        setCourseData((prev) => {
+            return { ...prev, [e.target.name]: e.target.value }
+        })
+
+    }
+
+    const handleOnSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(courseData)
+    }
 
     return (
         <div className="h-full py-4">
@@ -13,48 +43,113 @@ const CourseEdit = () => {
                 <p className="text-lg text-gray-600">Manage course details, curriculum and settings.</p>
             </div>
 
-            <div className="px-4 flex justify-end">
-                <button className="bg-blue-500 gap-2 rounded-md py-2 flex items-center justify-center text-white px-3 text-sm ">
-                    <Save className="" size={17} />
-                    Save Changes
-                </button>
-            </div>
+            <form onSubmit={handleOnSubmit} >
+                <div className="px-4 flex justify-end">
+                    <button className="bg-blue-500 gap-2 rounded-md py-2 flex items-center justify-center text-white px-3 text-sm ">
+                        <Save className="" size={17} />
+                        Save Changes
+                    </button>
+                </div>
+                <div className="px-4 py-6 shadow-sm m-2 bg-white rounded-md w-1/2">
+                    <p className="text-lg mb-4 font-bold">Course Information</p>
+                    <div className="mt-2">
+                        <Input
+                            value={courseData.courseName}
+                            name="courseName"
+                            className="border-gray-300" type="text"
+                            onChange={handleOnChange}
+                            placeholder='Full Stack'
+                            label='Course Name'
+                            required={true}
+                        />
 
-            <div className="px-4 py-6 shadow-sm m-2 bg-white rounded-md w-1/2">
-                <p className="text-lg mb-4 font-bold">Course Information</p>
-                <div className="mt-2">
-                    <Input disable={true} value={name} name="course Name" className="border-gray-300" type="text" onChange={(e) => { setName(e.target.value) }} placeholder='Full Stack' label='Course Name' required={true} />
+                        <div className="py-1 w-full"></div>
 
-                    <div className="py-1 w-full"></div>
+                        <p className="text-sm font-medium mt-2">Slug</p>
+                        <div className="flex w-full gap-1">
+                            <Input
+                                className="w-full border-gray-300"
+                                value={courseData.courseSlug}
+                                name="courseSlug"
+                                type="text"
+                                onChange={handleOnChange}
+                                placeholder='Full Stack'
+                                required={true}
+                            />
+                            <Input
+                                className="w-full"
+                                value={''}
+                                name="course Name"
+                                type="text"
+                                onChange={handleOnChange}
+                                placeholder='Full Stack'
+                                disable={true}
+                            />
+                        </div>
 
-                    <p className="text-sm font-medium mt-2">Slug</p>
-                    <div className="flex w-full gap-1">
-                        <Input className="w-full border-gray-300" value={name} name="course Name" type="text" onChange={(e) => { setName(e.target.value) }} placeholder='Full Stack' required={true} />
-                        <Input className="w-full " value={name} name="course Name" type="text" onChange={(e) => { setName(e.target.value) }} placeholder='Full Stack' required={true} />
-                    </div>
+                        <div className="py-1 w-full"></div>
 
-                    <div className="py-1 w-full"></div>
+                        <div className="flex items-end gap-2">
+                            <Input
+                                className=""
+                                value={courseData.courseDuration}
+                                name="courseDuration"
+                                type="number"
+                                onChange={handleOnChange}
+                                placeholder='duration in days'
+                                label="Course Duration"
+                                required={true}
+                            />
+                            <Input
+                                className="flex-1"
+                                name="days"
+                                onChange={handleOnChange}
+                                labelClassName=""
+                                placeholder='days'
+                                label=""
+                                value=""
+                                type="input"
+                                disable={true}
+                            />
 
-                    <div className="flex items-end gap-2">
-                        <Input className="" value={name} name="course Name" type="number" onChange={(e) => { setName(e.target.value) }} placeholder='Full Stack' label="Course Duration" required={true} />
-                        <SelectInput option={[{ label: 'Weeks', value: 'WK' }]} name="week" className="" />
-                        <Input className="flex-1" value={name} name="course Name" type="number" onChange={(e) => { setName(e.target.value) }} labelClassName="" placeholder='Weeks' label="Course Fee" required={true} />
-                    </div>
+                            <Input
+                                className="flex-1"
+                                value={courseData.courseFee}
+                                name="courseFee"
+                                type="number"
+                                onChange={handleOnChange}
+                                placeholder='5000'
+                                label="Course Fee"
+                                required={true}
+                            />
+                        </div>
 
-                    <div className="py-1 w-full"></div>
+                        <div className="py-1 w-full"></div>
+                        <div className="flex gap-2">
+                            <SelectInput
+                                option={courseStatus}
+                                name="courseStatus"
+                                className=""
+                                label="Course Status"
+                                onChange={handleOnChange}
+                            />
+                        </div>
 
-                    <div className="flex gap-2">
-                        <SelectInput option={[{ label: 'Weeks', value: 'WK' }]} name="week" className="" label="Course Status" />
-                        <SelectInput option={[{ label: 'Weeks', value: 'WK' }]} name="Wilson Yadav" className="" label="Course Trainer" />
-                    </div>
+                        <div className="py-1 w-full"></div>
 
-                    <div className="py-1 w-full"></div>
-
-                    <div>
-                        <Input label="Course Description" type="textarea" name="description" value={name} onChange={() => { }} placeholder="Course Description" />
+                        <div>
+                            <Input
+                                label="Course Description"
+                                type="textarea"
+                                name="courseDescription"
+                                value={courseData.courseDescription}
+                                onChange={handleOnChange}
+                                placeholder="Course Description"
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
