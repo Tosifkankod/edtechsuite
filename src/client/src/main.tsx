d101b38faafe88c71f714f084407004dcb493429
Tosif Kankod <73471650+Tosifkankod@users.noreply.github.com>
2025-12-30 14:55:09 +0530

    feat: added queryClient

--- src/client/src/main.tsx
+++ src/client/src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.tsx'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes.tsx'
+import { queryClient } from './utils/queryClient'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
