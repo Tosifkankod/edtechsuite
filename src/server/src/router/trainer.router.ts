9de143e66c4cf9b702ad6a009f28c278fe69d162
Tosif Kankod <73471650+Tosifkankod@users.noreply.github.com>
2026-02-10 23:21:57 +0530

    feat: trainer- added getsingle route

--- src/server/src/router/trainer.router.ts
+++ src/server/src/router/trainer.router.ts
@@ -8,6 +8,6 @@ const router = express.Router();
 
 router.route('/').get(validateResource(indexSchema), trainerController.index)
 router.route('/').post(validateResource(trainerSchema), trainerController.createTrainer)
-router.route('/:id').delete(trainerController.deleteTrainer).patch(trainerController.updateTrainer)
+router.route('/:id').delete(trainerController.deleteTrainer).patch(trainerController.updateTrainer).get(trainerController.singleTrainer)
 
 export default router;
